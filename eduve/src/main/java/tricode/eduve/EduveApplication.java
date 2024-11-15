4c4de2b075f0c06b36c5729e7e3dad3b329a284d
hyejin2234 <hhj020304@naver.com>
2024-11-15 14:23:02 +0900

    feat: 프로젝트 기본 세팅

---
+++ eduve/src/main/java/tricode/eduve/EduveApplication.java
@@ -0,0 +1,13 @@
+package tricode.eduve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduveApplication.class, args);
+	}
+
+}
---
+++ eduve/src/test/java/tricode/eduve/EduveApplicationTests.java
@@ -0,0 +1,13 @@
+package tricode.eduve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
