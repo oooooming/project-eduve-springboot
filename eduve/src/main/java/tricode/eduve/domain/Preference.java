b09b6a00b0700f5f0c037a037c5bdf87430e2a3f
oooooming <minjio030107@ewhain.net>
2024-11-15 16:08:52 +0900

    feat: Character, UserCharacter, Preference 도메인 세팅

--- eduve/src/main/java/tricode/eduve/domain/Preference.java
+++ eduve/src/main/java/tricode/eduve/domain/Preference.java
@@ -1,4 +1,27 @@
 package tricode.eduve.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Preference {
+
+    @Id
+    @Column(name = "preference_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int preferenceId;
+
+    @Column(nullable = false)
+    private String tone;
+
+    @Column(nullable = false)
+    private String descriptionLevel;
+
 }
--- eduve/src/main/java/tricode/eduve/domain/UserCharacter.java
+++ eduve/src/main/java/tricode/eduve/domain/UserCharacter.java
@@ -1,4 +1,24 @@
 package tricode.eduve.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCharacter {
+
+    @Id
+    @Column(name = "userCharacter_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userCharacterId;
+
+    @Column
+    private String userCharacterName;
+
 }
