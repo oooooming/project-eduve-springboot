9a29490762b2eee74e75d0d993b7c4b60dec707b
oooooming <minjio030107@ewhain.net>
2025-03-16 10:31:43 +0900

    feat: login dispatcher and DB based login

--- eduve/src/main/java/tricode/eduve/config/SecurityConfig.java
+++ eduve/src/main/java/tricode/eduve/config/SecurityConfig.java
@@ -2,15 +2,34 @@ package tricode.eduve.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import tricode.eduve.LoginFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean //암호화
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -39,6 +58,9 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join/teacher", "/join/student").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         //세션 설정
         http
--- eduve/src/main/java/tricode/eduve/repository/UserRepository.java
+++ eduve/src/main/java/tricode/eduve/repository/UserRepository.java
@@ -6,4 +6,7 @@ import tricode.eduve.domain.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
+
+    //username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
+    User findByUsername(String username);
 }
