8d559f3b9906a95c9543cc6a44912129de309e4d
oooooming <minjio030107@ewhain.net>
2025-03-24 21:43:24 +0900

    fix: join request body

--- eduve/src/main/java/tricode/eduve/controller/FileUploadController.java
+++ eduve/src/main/java/tricode/eduve/controller/FileUploadController.java
@@ -5,5 +5,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/files")
-public class FileController {
+public class FileUploadController {
 }
--- eduve/src/main/java/tricode/eduve/controller/StudentJoinController.java
+++ eduve/src/main/java/tricode/eduve/controller/StudentJoinController.java
@@ -1,10 +1,15 @@
 package tricode.eduve.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import tricode.eduve.dto.JoinDTO;
 import tricode.eduve.service.StudentJoinService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
@@ -19,11 +24,15 @@ public class StudentJoinController {
     }
 
     @PostMapping("/join/student")
-    public String joinProcess(JoinDTO joinDTO) {
+    public ResponseEntity<Map<String, Object>> joinProcess(@RequestBody JoinDTO joinDTO) {
 
         System.out.println(joinDTO.getUsername());
         studentJoinService.joinProcess(joinDTO);
 
-        return "ok";
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "회원가입이 완료되었습니다.");
+        response.put("userId", joinDTO.getUsername());
+
+        return ResponseEntity.ok(response);
     }
 }
