274e3f762a202c22c24ffa31eaedefef0acaa257
hyejin2234 <hhj020304@ewhain.net>
2025-04-03 18:05:39 +0900

    feat: folder API

---
+++ eduve/src/main/java/tricode/eduve/repository/FolderRepository.java
@@ -0,0 +1,12 @@
+package tricode.eduve.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tricode.eduve.domain.Folder;
+
+import java.util.Optional;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+    Optional<Folder> findByPath(String path);
+}
