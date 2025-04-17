dce169a231f1005967d018e98fac627367e79208
oooooming <minjio030107@ewhain.net>
2025-04-17 21:43:54 +0900

    feat: get use,r name update, delete user

---
+++ eduve/src/main/java/tricode/eduve/dto/response/UserResponseDto.java
@@ -0,0 +1,35 @@
+package tricode.eduve.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import tricode.eduve.domain.User;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserResponseDto {
+    private Long userId;
+    private String username;
+    private String name;
+    private String email;
+    private String role;
+
+    @Builder
+    public UserResponseDto(Long userId, String username, String name, String email, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public static UserResponseDto from(User user) {
+        return UserResponseDto.builder()
+                .userId(user.getUserId())
+                .username(user.getUsername())
+                .name(user.getName())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .build();
+    }
+}
