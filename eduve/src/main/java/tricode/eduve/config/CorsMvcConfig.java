5da9011a4fe8361e404ba7fe090f6264206d15df
oooooming <minjio030107@ewhain.net>
2025-05-18 02:19:12 +0900

    CORS 설정 추가

--- eduve/src/main/java/tricode/eduve/config/CorsMvcConfig.java
+++ eduve/src/main/java/tricode/eduve/config/CorsMvcConfig.java
@@ -10,6 +10,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     }
 }
